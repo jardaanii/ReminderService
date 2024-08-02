adde4087ec42176ab40d9373572e8c5aed988ec0
Akash <kiraacodes@gmail.com>
2024-08-02 23:46:36 +0530

    Added rabbit mq subscriber

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const EmailService = require("./services/email-service");
 const apiRoutes = require("./routes/index");
+
+const { createChannel, subscribeMessage } = require("./utils/messageQueue");
+const { REMINDER_BINDING_KEY } = require("./config/serverConfig");
 const { sendBasicEmail } = require("./services/email-service");
 const jobs = require("./utils/job");
 
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
   const app = express();
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  const channel = await createChannel();
+
+  subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
   app.use("/api", apiRoutes);
 
   app.listen(PORT, () => {
